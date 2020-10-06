362f962449afe28ff233f2b3b14bd3cd614db344
Ayush Sahu <ayushsahu1@yahoo.com>
2020-10-07 00:19:49 +0530

    Add for loop version

--- Patterns/Pyramid Number Pattern.py
+++ Patterns/Pyramid Number Pattern.py
@@ -53,3 +53,20 @@ while i <= n:
     #
     print()
     i += 1
+   
+   #### OR
+   '''
+   n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(' ',end='')
+    end=i
+    for k in range(i,0,-1):
+        print(i,end='')
+        i-=1
+    
+    for j in range(2, end+1):
+        print(j,end='')       
+    i+=1   
+    print()
+'''
